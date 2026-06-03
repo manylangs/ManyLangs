b2e5f366b88028072447f7a0b3972fea0468ea88
Hasuk Jung <manylangs.help@gmail.com>
2026-06-04 00:14:56 +0900

    fix: real audio controller sticky wrapper 제거

--- components/audio/controllers/RealAudioController.tsx
+++ components/audio/controllers/RealAudioController.tsx
@@ -7,23 +7,19 @@ type Props = {
 };
 
 export default function RealAudioController({ src }: Props) {
-  const controllerHeight = 56;
-
   return (
-    <div style={{ minHeight: controllerHeight, marginBottom: 12 }}>
-      <div
-        style={{
-          position: "sticky",
-          top: 100,
-          zIndex: 900,
-          background: "#fff",
-          paddingTop: 8,
-          paddingBottom: 8,
-          borderBottom: "1px solid #eee",
-        }}
-      >
-        <AudioPlayer key={src} src={src} />
-      </div>
-    </div>
+    <section
+      style={{
+        position: "sticky",
+        top: 100,
+        zIndex: 900,
+        background: "#fff",
+        paddingTop: 8,
+        paddingBottom: 8,
+        borderBottom: "1px solid #eee",
+      }}
+    >
+      <AudioPlayer key={src} src={src} />
+    </section>
   );
 }
