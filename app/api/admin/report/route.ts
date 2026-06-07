02a84637a773f1429e169ec9d582e10d7f977bce
Hasuk Jung <manylangs.help@gmail.com>
2026-06-08 01:51:03 +0900

    Fix Stripe API version

--- app/api/admin/report/route.ts
+++ app/api/admin/report/route.ts
@@ -2,7 +2,7 @@ import { NextRequest, NextResponse } from "next/server";
 import Stripe from "stripe";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: "2025-05-28.basil",
+  apiVersion: "2026-01-28.clover",
 });
 
 export async function GET(req: NextRequest) {
