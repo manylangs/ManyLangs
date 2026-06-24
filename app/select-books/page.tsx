e3f3cd98ebca30f411d5add3db44fe228e5fb389
Hasuk Jung <manylangs.help@gmail.com>
2026-06-24 21:10:40 +0900

    update ios purchase messaging

--- app/select-books/page.tsx
+++ app/select-books/page.tsx
@@ -1280,7 +1280,6 @@ export default function SelectBooksPage() {
                         </p>
                       </div>
                     )}
-                    )}
 
                   </div>
 
