95553ac9014d5d094b1360bbe623c57b41f0de62
Hasuk Jung <manylangs.help@gmail.com>
2026-06-24 20:56:05 +0900

    update landing demo library screenshot

--- app/select-books/page.tsx
+++ app/select-books/page.tsx
@@ -1255,14 +1255,14 @@ export default function SelectBooksPage() {
                     >
                       Buy Coupons
                     </button>
-                    <div className="flex items-center justify-center h-full text-center pt-3">
+                    {/* <div className="flex items-center justify-center h-full text-center pt-3">
                       <p className="text-xs text-gray-500 leading-relaxed">
                         1 coupon = 30-day access
                         <br />Coupons can be shared with others.
                         <br />However, ManyLangs cannot individually track
                         <br />whether a shared coupon has been used.
                       </p>
-                    </div>
+                    </div> */}
 
                   </div>
 
