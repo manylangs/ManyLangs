debd8bbe13d35c736a62e7f61155bd8e15409138
Hasuk Jung <manylangs.help@gmail.com>
2026-06-29 23:43:31 +0900

    add language configuration

--- app/config/languages.ts
+++ app/config/languages.ts
@@ -1,6 +1,6 @@
 export const LANGUAGES = [
   { code: "kr", label: "Korean" },
-  //{ code: "en", label: "English" },
+  { code: "en", label: "English" },
   //{ code: "es", label: "Spanish" },
   //{ code: "fr", label: "French" },
   //{ code: "pt", label: "Portuguese" },
