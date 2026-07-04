d43e8ff2195e1e717c9951209a210c5385507f49
Hasuk Jung <manylangs.help@gmail.com>
2026-07-05 03:03:11 +0900

    enable es, fr, pt in LANGUAGES config

--- app/config/languages.ts
+++ app/config/languages.ts
@@ -1,9 +1,9 @@
 export const LANGUAGES = [
   { code: "kr", label: "Korean" },
   { code: "en", label: "English" },
-  //{ code: "es", label: "Spanish" },
-  //{ code: "fr", label: "French" },
-  //{ code: "pt", label: "Portuguese" },
+  { code: "es", label: "Spanish" },
+  { code: "fr", label: "French" },
+  { code: "pt", label: "Portuguese" },
 ];
 
 // 자동 파생
